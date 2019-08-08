61cf0e068300e69ce77a38d8e3ad0b3671bf793c
kFYatek <kfyatek@gmail.com>
2019-08-08 22:28:39 +0200

    Extracted ImageModem

--- color_modem/cli.py
+++ color_modem/cli.py
@@ -1,55 +1,14 @@
 # -*- coding: utf-8 -*-
-import itertools
 import sys
 
-import numpy
 from PIL import Image
 
 from color_modem.comb import Simple3DCombModem
+from color_modem.image import ImageModem
 from color_modem.niir import NiirModem
 from color_modem.ntsc import NtscCombModem, NtscVariant, NtscModem
 from color_modem.pal import Pal3DModem, PalVariant, PalDModem, PalSModem
 from color_modem.secam import SecamModem
-
-
-def modulate(modem, img, frame):
-    assert img.width == 720
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
-    r = bytearray(img.getdata(0))
-    g = bytearray(img.getdata(1))
-    b = bytearray(img.getdata(2))
-    output = numpy.zeros(720 * img.height, dtype=numpy.uint8)
-    for y in itertools.chain(range(0, img.height, 2), range(1, img.height, 2)):
-        output[720 * y:720 * (y + 1)] = modem.encode_composite_level(
-            modem.modulate(frame, y, r[720 * y:720 * (y + 1)], g[720 * y:720 * (y + 1)], b[720 * y:720 * (y + 1)]))
-
-    return Image.frombytes('L', img.size, bytes(bytearray(output)))
-
-
-def demodulate(modem, img, frame):
-    def as_bytes(array):
-        return numpy.uint8(numpy.rint(numpy.maximum(numpy.minimum(array, 255.0), 0.0)))
-
-    assert img.width == 720
-    if img.mode != 'L':
-        img = img.convert('L')
-    composite = modem.decode_composite_level(numpy.array(img.getdata()))
-    output = numpy.zeros(720 * img.height * 3, dtype=numpy.uint8)
-    demodulation_delay = getattr(modem, 'demodulation_delay', 0)
-    for field in range(2):
-        for y in range(field, 2 * demodulation_delay, 2):
-            modem.demodulate(frame, y, composite[720 * y:720 * (y + 1)])
-        for y in range(field, img.height, 2):
-            input_y = y + 2 * demodulation_delay
-            while input_y >= img.height:
-                input_y -= 2
-            rLine, gLine, bLine = modem.demodulate(frame, y + 2 * demodulation_delay,
-                                                   composite[720 * input_y:720 * (input_y + 1)])
-            output[3 * 720 * y:3 * 720 * (y + 1):3] = as_bytes(rLine)
-            output[3 * 720 * y + 1:3 * 720 * (y + 1) + 1:3] = as_bytes(gLine)
-            output[3 * 720 * y + 2:3 * 720 * (y + 1) + 2:3] = as_bytes(bLine)
-    return Image.frombytes('RGB', img.size, bytes(bytearray(output)))
 
 
 def main():
@@ -75,8 +34,9 @@ def main():
     #### NIIR (SECAM IV)
     # modem = NiirModem()
 
+    img_modem = ImageModem(modem)
     img = Image.open(sys.argv[1])
-    img = modulate(modem, img, 0)
+    img = img_modem.modulate(img, 0)
     img.save(sys.argv[2])
-    img = demodulate(modem, img, 0)
+    img = img_modem.demodulate(img, 0)
     img.save(sys.argv[3])
---
+++ color_modem/image.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import itertools
+
+import numpy
+from PIL import Image
+
+
+class ImageModem(object):
+    def __init__(self, modem):
+        self._modem = modem
+
+    def modulate(self, img, frame=0):
+        assert img.width == 720
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        r = bytearray(img.getdata(0))
+        g = bytearray(img.getdata(1))
+        b = bytearray(img.getdata(2))
+        output = numpy.zeros(720 * img.height, dtype=numpy.uint8)
+        for y in itertools.chain(range(0, img.height, 2), range(1, img.height, 2)):
+            output[720 * y:720 * (y + 1)] = self._modem.encode_composite_level(
+                self._modem.modulate(frame, y, r[720 * y:720 * (y + 1)], g[720 * y:720 * (y + 1)],
+                                     b[720 * y:720 * (y + 1)]))
+        return Image.frombytes('L', img.size, bytes(bytearray(output)))
+
+    def demodulate(self, img, frame=0):
+        def as_bytes(array):
+            return numpy.uint8(numpy.rint(numpy.maximum(numpy.minimum(array, 255.0), 0.0)))
+
+        assert img.width == 720
+        if img.mode != 'L':
+            img = img.convert('L')
+        composite = self._modem.decode_composite_level(numpy.array(img.getdata()))
+        output = numpy.zeros(720 * img.height * 3, dtype=numpy.uint8)
+        demodulation_delay = getattr(self._modem, 'demodulation_delay', 0)
+        for field in range(2):
+            for y in range(field, 2 * demodulation_delay, 2):
+                self._modem.demodulate(frame, y, composite[720 * y:720 * (y + 1)])
+            for y in range(field, img.height, 2):
+                input_y = y + 2 * demodulation_delay
+                while input_y >= img.height:
+                    input_y -= 2
+                rLine, gLine, bLine = self._modem.demodulate(frame, y + 2 * demodulation_delay,
+                                                             composite[720 * input_y:720 * (input_y + 1)])
+                output[3 * 720 * y:3 * 720 * (y + 1):3] = as_bytes(rLine)
+                output[3 * 720 * y + 1:3 * 720 * (y + 1) + 1:3] = as_bytes(gLine)
+                output[3 * 720 * y + 2:3 * 720 * (y + 1) + 2:3] = as_bytes(bLine)
+        return Image.frombytes('RGB', img.size, bytes(bytearray(output)))
