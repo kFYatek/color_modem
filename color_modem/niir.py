d85a428c18539f03d0769a13e7345eceef1b03a6
kFYatek <kfyatek@gmail.com>
2019-08-10 02:45:43 +0200

    Made NIIR support variants

--- color_modem/niir.py
+++ color_modem/niir.py
@@ -3,24 +3,19 @@
 import numpy
 import scipy.signal
 
-from color_modem import utils
+from color_modem import utils, pal
 
 
 class NiirModem:
-    def __init__(self, fs=13500000.0):
-        self._carrier_phase_step = numpy.pi * 4433618.75 / fs
+    def __init__(self, config=pal.PalVariant.PAL, fs=13500000.0):
+        self._carrier_phase_step = numpy.pi * config.fsc / fs
+        self.config = config
 
-        line_shift_by_pi = ((2.0 * 4433618.75) / (25.0 * 625.0)) % 2.0
-        self.line_shift = numpy.pi * line_shift_by_pi
-        odd_numbered_digital_line_shift_by_pi = (line_shift_by_pi * 313) % 2.0
-        self._odd_numbered_digital_line_shift = numpy.pi * odd_numbered_digital_line_shift_by_pi
-        frame_shift_by_pi = (line_shift_by_pi * 625) % 2.0
-        self._frame_shift = numpy.pi * frame_shift_by_pi
-
-        self._chroma_precorrect_lowpass = utils.iirdesign(2.0 * 1300000.0 / fs, 2.0 * 4000000.0 / fs, 3.0, 20.0)
+        self._chroma_precorrect_lowpass = utils.iirdesign(2.0 * config.bandwidth3db / fs,
+                                                          2.0 * config.bandwidth20db / fs, 3.0, 20.0)
         self._demodulate_resample_factor = 4
         self._demodulate_upsampled_baseband_filter, self._demodulate_upsampled_filter = NiirModem._demodulate_am_design(
-            2.0 * 4433618.75 / fs, 2.0 * 1300000.0 / fs, 2.0 * 4000000.0 / fs, 3.0, 20.0,
+            2.0 * config.fsc / fs, 2.0 * config.bandwidth3db / fs, 2.0 * config.bandwidth20db / fs, 3.0, 20.0,
             self._demodulate_resample_factor)
 
         self._last_frame = -1
@@ -49,37 +44,11 @@ class NiirModem:
         b = luma - 2.03 * db
         return r, g, b
 
-    def _calculate_start_phase(self, frame, line):
-        frame %= 4
-        frame_shift = (frame * self._frame_shift) % (2.0 * numpy.pi)
-        field_shift = (line % 2) * self._odd_numbered_digital_line_shift
-        line_shift = ((line // 2) * self.line_shift) % (2.0 * numpy.pi)
-        return (frame_shift + field_shift + line_shift) % (2.0 * numpy.pi)
-
-    def _is_alternate_line(self, frame, line):
-        # For 525-line system:
-        #
-        # NOTE: Lines 21-22, 283-285 and 263 are defined to be active by the
-        # analogue specifications, but are by convention not encoded digitally.
-        #
-        # digital line 0 ~ analog line 23 (odd fields 1 & 3)
-        # digital line 1 ~ analog line 286 (even fields 2 & 4)
-        # digital line 2 - analog line 24 (odd fields 1 & 3)
-        # digital line 3 - analog line 287 (even fields 2 & 4)
-        #
-        # For 625-line system:
-        #
-        # digital line 0 ~ analog line 23 (even fields 1 & 3)
-        # digital line 1 ~ analog line 336 (odd fields 2 & 4)
-        # digital line 2 - analog line 24 (even fields 1 & 3)
-        # digital line 3 - analog line 337 (odd fields 2 & 4)
-        return ((line % 2) ^ ((line // 2) % 2)) != frame % 2
-
     def _modulate_precorrected_chroma(self, frame, line, updb, updr):
-        start_phase = self._calculate_start_phase(frame, line)
+        start_phase = self.config.start_phase(frame, line)
         phase = numpy.linspace(start=start_phase, stop=start_phase + len(updb) * 2.0 * self._carrier_phase_step,
                                num=len(updb), endpoint=False) % (2.0 * numpy.pi)
-        if not self._is_alternate_line(frame, line):
+        if not self.config.is_alternate_line(frame, line):
             return updb * numpy.sin(phase) + updr * numpy.cos(phase)
         else:
             return -numpy.sqrt(updb * updb + updr * updr) * numpy.sin(phase)
@@ -119,20 +88,17 @@ class NiirModem:
         saturation_plus_ep_up = self._demodulate_upsampled_baseband_filter(demod_up)
         phasemod_up = modulated_up / saturation_plus_ep_up
 
-        if not self._is_alternate_line(frame, line):
+        if not self.config.is_alternate_line(frame, line):
             carrier_up = self._last_phasemod_up
             huemod_up = phasemod_up
+            shift = self.config.line_shift
         else:
             carrier_up = phasemod_up
             huemod_up = self._last_phasemod_up
-
+            shift = -self.config.line_shift
 
         shifted_carrier_up = carrier_up[0:-1] + carrier_up[1:]
-        altcarrier_up = -numpy.concatenate((numpy.zeros(1), numpy.diff(shifted_carrier_up), numpy.zeros(1)))
-
-        if not self._is_alternate_line(frame, line):
-            carrier_up = -carrier_up
-            altcarrier_up = -altcarrier_up
+        altcarrier_up = numpy.concatenate((numpy.zeros(1), numpy.diff(shifted_carrier_up), numpy.zeros(1)))
 
         cosphi_up = huemod_up * carrier_up
         sinphi_up = huemod_up * altcarrier_up
@@ -142,15 +108,17 @@ class NiirModem:
         normalizer = numpy.sqrt(sinphi * sinphi + cosphi * cosphi)
         sinphi /= normalizer
         cosphi /= normalizer
-        phi = numpy.arctan2(sinphi, cosphi)
+
+        cosphi, sinphi = cosphi * numpy.cos(shift) - sinphi * numpy.sin(shift), \
+                         cosphi * numpy.sin(shift) + sinphi * numpy.cos(shift)
 
         saturation_plus_ep = scipy.signal.resample_poly(saturation_plus_ep_up, up=1,
                                                         down=self._demodulate_resample_factor)
         saturation = numpy.maximum(saturation_plus_ep - 25.5, 0.0)
-        db = saturation * sinphi
-        dr = saturation * cosphi
-        updb = saturation_plus_ep * sinphi
-        updr = saturation_plus_ep * cosphi
+        db = saturation * cosphi
+        dr = saturation * sinphi
+        updb = saturation_plus_ep * cosphi
+        updr = saturation_plus_ep * sinphi
         luma = composite - self._modulate_precorrected_chroma(frame, line, updb, updr)
 
         self._last_phasemod_up = phasemod_up
